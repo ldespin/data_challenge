6d480e0233cf11a843ea757b79a13818bef7e248
ldespin <lucas.despin@centrale.centralelille.fr>
2022-02-23 09:52:23 +0100

    Notebook propre

--- functions.py
+++ functions.py
@@ -1,7 +1,12 @@
 import numpy as np
 import cv2
+import pandas as pd
 
 def get_imflow(img_0, img_1):
+
+    ''''
+    Compute the optical flow between two images an return it
+    '''
 
     # Creates an image filled with zero
     # intensities with the same dimensions
@@ -16,3 +21,12 @@ def get_imflow(img_0, img_1):
                                        0.5, 3, 15, 3, 5, 1.2, 0)
     
     return flow
+
+def write_results(prediction, filename):
+    df = pd.DataFrame(columns = ['Id', 'expression'])
+    df['expression'] = prediction
+    df['Id'] = df.index
+    results = df.to_csv(index = False)
+    f = open(filename, 'w')
+    f.write(results)
+    f.close()
