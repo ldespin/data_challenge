487827378ce56ef28d50f31e9590282986b32199
ldespin <lucas.despin@centrale.centralelille.fr>
2022-02-24 08:41:51 +0100

    Fine tune

--- functions.py
+++ functions.py
@@ -20,7 +20,26 @@ def get_imflow(img_0, img_1):
                                        None,
                                        0.5, 3, 15, 3, 5, 1.2, 0)
     
-    return flow
+    return np.array(cv2.cartToPolar(flow[..., 0], flow[..., 1]))
+
+def mag_ang_std_mean(flows):
+    mags = []
+    angs = []
+    for flow in flows:
+        mag, ang = flow[0], flow[1]
+        mags = np.concatenate((mags,mag.flatten()))
+        angs = np.concatenate((angs,ang.flatten()))
+
+    mean_mags = np.mean(mags)
+    std_mags = np.std(mags)
+
+    mean_angs = np.mean(angs)
+    std_angs = np.std(angs)
+
+    return mean_mags, std_mags, mean_angs, std_angs
+
+
+    return norm_flow
 
 def write_results(prediction, filename):
     df = pd.DataFrame(columns = ['Id', 'expression'])
